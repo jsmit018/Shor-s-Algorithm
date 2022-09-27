93a83194b6b8cebc1d0bcae494675a9b8a9b87ef
jsmit018 <56923368+jsmit018@users.noreply.github.com>
2022-09-27 09:24:18 -0400

    Add files via upload
    Added comments in the code and adjusted print statements

--- shors.py
+++ shors.py
@@ -17,40 +17,54 @@
 import math
 from random import Random, randint, random
 
+# Helper function, calls gcd function to find the gcd between two numbers
 def find_gcd(number_1, number_2):
     return math.gcd(number_1, number_2)
 
+# Shor's Algorithm implemented in the below function
 def shors_algorithm(number_to_factor):
+    # The Number that is trying to be factored
     N = number_to_factor
     print("The number is: {}".format(N))
+    # The Number we are utilizing to find factors of N
     K = randint(1, number_to_factor)
     print("The factor number is: {}".format(K))
+    # Calling the gcd helper function to find the gcd
     GCD = find_gcd(N, K)
     print("The GCD is {}".format(GCD))
+    # If the GCD returned is not one, that number is a factor of N
     if GCD != 1:
         print("{} is a factor of {}".format(GCD, N))
     else:
+        # Otherwise, we have to step into Shor's Algorithm
+        # Initialize an array to hold the results of the transformation, a variable q and the number of times the transformation was conducted
         transformations = []
         q = 1
         iterations = 0
+        # Perform the transformation sequence until the remainder is one
         while True:
             q = (q*K) % N
             transformations.append(q)
             iterations += 1
-            print("This iteration, remainder, and N: {} {} {} {}".format(iterations, q, N, K))
+            print("This iteration, remainder, N, and factor: {} {} {} {}".format(iterations, q, N, K))
             if q == 1:
                 break
+        # If the number of iterations is odd then we have to redo the algorithm with a different K value
         if iterations % 2 != 0:
-            print("Reloading here")
+            print("Number of Iterations is not even")
             shors_algorithm(N)
         else:
+            # We now grab the result of the iterations/2 th transformation
             p = transformations[int((iterations/2) -1)]
             print("This is p {}".format(p))
+            # if the P + 1 is equal to the N then we have to redo the algorithm with a different K value
             if p + 1 == N:
-                print("Reloading")
+                print("P + 1 == N, recalling function")
                 shors_algorithm(N)
             else:
-                print("This is the {} transformation".format(iterations/2))
+                # Otherwise, we identify the transformation
+                print("This is the {} transformation".format(int(iterations/2)))
+                # Utilizing the gcd function, with P-1 and P+1 will return 2 factors of the number N
                 print("{} and {} are factors of {}".format(find_gcd(p+1, N), find_gcd(p-1,N), N))
         
 
